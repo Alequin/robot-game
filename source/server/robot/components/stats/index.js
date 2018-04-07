b6d3962ad97a292a458cf3be2f1e7d45dffc75ed
James Cox <j-cox03@hotmail.com>
2018-04-07 10:15:59 +0100

    added more shield related stats

--- source/server/robot/components/stats/index.js
+++ source/server/robot/components/stats/index.js
@@ -1,8 +1,12 @@
 export const stats = Object.freeze({
     ARMOUR: "Armour",
-    SHIELD: "Shield",
     WEIGHT: "Weight",
     AGILITY: "Agility",
+
+    SHIELD_CHARGE_RATE: "Shield charge rate",
+    SHIELD_EFFICIENCY: "Shield efficiency",
+    SHIELD_CAPACITY: "Shield capacity",
+
     BATTERY_CHARGE_RATE: "Battery charge rate",
     BATTERY_EFFICIENCY: "Battery efficiency",
     BATTERY_CAPACITY: "Battery capacity"
@@ -12,9 +16,13 @@ export const newStats = (initalStats = {}) => {
 
   const {
     [stats.ARMOUR]: armour,
-    [stats.SHIELD]: shield,
     [stats.WEIGHT]: weight,
     [stats.AGILITY]: agility,
+
+    [stats.SHIELD_CHARGE_RATE]: shieldChargeRate,
+    [stats.SHIELD_EFFICIENCY]: shieldEfficiency,
+    [stats.SHIELD_CAPACITY]: shieldCapacity,
+
     [stats.BATTERY_CHARGE_RATE]: batteryChargeRate,
     [stats.BATTERY_EFFICIENCY]: batteryEfficiency,
     [stats.BATTERY_CAPACITY]: batteryCapacity
@@ -22,9 +30,13 @@ export const newStats = (initalStats = {}) => {
 
   return {
     [stats.ARMOUR]: armour || 0,
-    [stats.SHIELD]: shield || 0,
     [stats.WEIGHT]: weight || 0,
     [stats.AGILITY]: agility || 0,
+
+    [stats.SHIELD_CHARGE_RATE]: shieldChargeRate || 0,
+    [stats.SHIELD_EFFICIENCY]: shieldEfficiency || 0,
+    [stats.SHIELD_CAPACITY]: shieldCapacity || 0,
+
     [stats.BATTERY_CHARGE_RATE]: batteryChargeRate || 0,
     [stats.BATTERY_EFFICIENCY]: batteryEfficiency || 0,
     [stats.BATTERY_CAPACITY]: batteryCapacity || 0
--- source/server/robot/components/stats/new-stats.test.js
+++ source/server/robot/components/stats/new-stats.test.js
@@ -4,9 +4,13 @@ describe("source/server/robot/components/stats/stats.js", () => {
   test("Should return a blank slate stats object when no inital values are given", () => {
     const expected = {
       [stats.ARMOUR]: 0,
-      [stats.SHIELD]: 0,
       [stats.WEIGHT]: 0,
       [stats.AGILITY]: 0,
+
+      [stats.SHIELD_CHARGE_RATE]: 0,
+      [stats.SHIELD_EFFICIENCY]: 0,
+      [stats.SHIELD_CAPACITY]: 0,
+
       [stats.BATTERY_CHARGE_RATE]: 0,
       [stats.BATTERY_EFFICIENCY]: 0,
       [stats.BATTERY_CAPACITY]: 0
@@ -16,39 +20,71 @@ describe("source/server/robot/components/stats/stats.js", () => {
   })
 
   describe("When given inital values", () => {
-    test("Should set armour, shield and weight based on the inital values given", () => {
+    test("Should set armour, weight and agility based on the inital values given", () => {
       const expected = {
         [stats.ARMOUR]: 100,
-        [stats.SHIELD]: 50,
         [stats.WEIGHT]: 25,
-        [stats.AGILITY]: 0,
+        [stats.AGILITY]: 50,
+
+        [stats.SHIELD_CHARGE_RATE]: 0,
+        [stats.SHIELD_EFFICIENCY]: 0,
+        [stats.SHIELD_CAPACITY]: 0,
+
         [stats.BATTERY_CHARGE_RATE]: 0,
         [stats.BATTERY_EFFICIENCY]: 0,
         [stats.BATTERY_CAPACITY]: 0
       }
       const actual = newStats({
         [stats.ARMOUR]: 100,
-        [stats.SHIELD]: 50,
         [stats.WEIGHT]: 25,
+        [stats.AGILITY]: 50,
       })
       expect(actual).toEqual(expected)
     })
 
-    test("Should set agility, battery-charge-rate and battery-capacity based on the inital values given", () => {
+    test(`Should set shield-capacity, shield-charge-rate and
+      shield-charge-capacity based on the inital values given`, () => {
+
       const expected = {
         [stats.ARMOUR]: 0,
-        [stats.SHIELD]: 0,
         [stats.WEIGHT]: 0,
-        [stats.AGILITY]: 100,
-        [stats.BATTERY_CHARGE_RATE]: 50,
-        [stats.BATTERY_EFFICIENCY]: 200,
-        [stats.BATTERY_CAPACITY]: 25
+        [stats.AGILITY]: 0,
+
+        [stats.SHIELD_CHARGE_RATE]: 100,
+        [stats.SHIELD_EFFICIENCY]: 50,
+        [stats.SHIELD_CAPACITY]: 20,
+
+        [stats.BATTERY_CHARGE_RATE]: 0,
+        [stats.BATTERY_EFFICIENCY]: 0,
+        [stats.BATTERY_CAPACITY]: 0
       }
       const actual = newStats({
-        [stats.AGILITY]: 100,
-        [stats.BATTERY_CHARGE_RATE]: 50,
-        [stats.BATTERY_CAPACITY]: 25,
-        [stats.BATTERY_EFFICIENCY]: 200,
+        [stats.SHIELD_CHARGE_RATE]: 100,
+        [stats.SHIELD_EFFICIENCY]: 50,
+        [stats.SHIELD_CAPACITY]: 20,
+      })
+      expect(actual).toEqual(expected)
+    })
+
+    test(`Should set battery-capacity, battery-charge-rate and
+      battery-charge-capacity based on the inital values given`, () => {
+      const expected = {
+        [stats.ARMOUR]: 0,
+        [stats.WEIGHT]: 0,
+        [stats.AGILITY]: 0,
+
+        [stats.SHIELD_CHARGE_RATE]: 0,
+        [stats.SHIELD_EFFICIENCY]: 0,
+        [stats.SHIELD_CAPACITY]: 0,
+
+        [stats.BATTERY_CHARGE_RATE]: 100,
+        [stats.BATTERY_EFFICIENCY]: 50,
+        [stats.BATTERY_CAPACITY]: 20
+      }
+      const actual = newStats({
+        [stats.BATTERY_CHARGE_RATE]: 100,
+        [stats.BATTERY_EFFICIENCY]: 50,
+        [stats.BATTERY_CAPACITY]: 20,
       })
       expect(actual).toEqual(expected)
     })
